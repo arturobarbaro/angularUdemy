6cb0ae157e7b0342fce17085be17d9c479612e1f
Arturo Barba <arturo.barba@iesdonana.org>
2019-03-16 20:38:34 +0100

    rutas hijas

--- 06-miscalaneos/src/app/app.routes.ts
+++ 06-miscalaneos/src/app/app.routes.ts
@@ -1,10 +1,22 @@
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { UsuariosComponent } from './components/usuarios/usuarios.component';
+import { UsuariosNuevoComponent } from './components/usuarios/usuarios-nuevo.component';
+import { UsuariosEditarComponent } from './components/usuarios/usuarios-editar.component';
+import { UsuariosDetalleComponent } from './components/usuarios/usuarios-detalle.component';
 
 const APP_ROUTER: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'usuario', component: UsuariosComponent },
+  {
+      path: 'usuario/:id',
+      component: UsuariosComponent,
+      children: [
+          { path: 'nuevo', component: UsuariosNuevoComponent },
+          { path: 'editar', component: UsuariosEditarComponent },
+          { path: 'detalle', component: UsuariosDetalleComponent },
+          { path: '**', pathMatch: 'full', redirectTo: 'nuevo' }
+      ]
+   },
   { path: '**', pathMatch: 'full', redirectTo: '' }
 ];
 
