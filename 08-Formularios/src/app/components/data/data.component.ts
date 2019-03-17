430aae86e34f011045925b14d7c38bef820cf35d
Arturo Barba <arturo.barba@iesdonana.org>
2019-03-17 16:50:02 +0100

    Validaciones en data

--- 08-Formularios/src/app/components/data/data.component.ts
+++ 08-Formularios/src/app/components/data/data.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-data',
@@ -7,7 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataComponent implements OnInit {
 
-  constructor() { }
+  forma:FormGroup;
+
+  constructor() {
+    this.forma=new FormGroup({
+            'nombre': new FormControl('', [
+                Validators.required,
+                Validators.minLength(3)
+            ]
+        ),
+        'apellido': new FormControl('', Validators.required ),
+        'email': new FormControl('',
+            [
+                Validators.required,
+                Validators.pattern("[A-Za-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")
+            ]
+        )
+    });
+  }
+
+  guardarCambios(){
+
+  }
 
   ngOnInit() {
   }
