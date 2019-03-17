66a2385587ceaa369222f8522a48f0876c48c935
Arturo Barba <arturo.barba@iesdonana.org>
2019-03-17 14:33:04 +0100

    Validaciones en cliente

--- 08-Formularios/src/app/components/template/template.component.ts
+++ 08-Formularios/src/app/components/template/template.component.ts
@@ -4,14 +4,18 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
-  styles: []
+  styles: [`
+       .ng-invalid.ng-touched:not(form) {
+           border: 1px solid red
+       }
+      `]
 })
 export class TemplateComponent {
 
   usuario:Object = {
-      nombre: "BBB",
-      apellido: "AAA",
-      email: ""
+      nombre: null,
+      apellido: null,
+      email: null
   }
 
   constructor() { }
