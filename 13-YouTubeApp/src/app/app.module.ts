837e2f4d0262d412cad2265222ed234f6146e3d3
Arturo Barba <arturo.barba@iesdonana.org>
2019-03-18 16:37:30 +0100

    Componentes

--- 13-YouTubeApp/src/app/app.module.ts
+++ 13-YouTubeApp/src/app/app.module.ts
@@ -2,15 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+//Servicios
+import { YoutubeService } from './providers/youtube.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+      YoutubeService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
