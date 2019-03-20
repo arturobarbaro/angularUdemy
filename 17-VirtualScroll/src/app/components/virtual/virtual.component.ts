c2bd42a4a6b46ece79c1acdccdc69e37751fcd84
Arturo Barba <arturo.barba@iesdonana.org>
2019-03-20 10:51:27 +0100

    Drag con cdk

--- 17-VirtualScroll/src/app/components/virtual/virtual.component.ts
+++ 17-VirtualScroll/src/app/components/virtual/virtual.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'app-virtual',
@@ -7,12 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VirtualComponent implements OnInit {
 
+  @ViewChild(CdkVirtualScrollViewport) viewport: CdkVirtualScrollViewport;
+
   personas = Array(500).fill(0);
 
   constructor() { }
 
   ngOnInit() {
-      console.log(this.personas)
+      console.log(this.personas);
+  }
+
+  final(){
+      this.viewport.scrollToIndex(this.personas.length-1)
+  }
+  principio(){
+      this.viewport.scrollToIndex(0);
   }
 
 }
